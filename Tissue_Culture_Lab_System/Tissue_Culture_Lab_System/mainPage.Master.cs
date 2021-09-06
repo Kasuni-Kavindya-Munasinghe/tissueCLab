e5dacd67eb8fab3f2e4c69f82048abcc30563f5e
HashiniJayasinghe <71764759+JAHNMDJM@users.noreply.github.com>
2021-09-06 22:29:36 +0400

    master page created

---
+++ Tissue_Culture_Lab_System/Tissue_Culture_Lab_System/mainPage.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tissue_Culture_Lab_System
+{
+    public partial class mainPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
