5ba1a33a9beb00d12d9fd02adb6f8e48eba51f40
virajsaranga <89581896+virajsaranga@users.noreply.github.com>
2021-10-03 15:21:22 +0530

    production management unit (viraj)

--- Tissue_Culture_Lab_System/Tissue_Culture_Lab_System/content folder/pdmViewPlan.aspx.cs
+++ Tissue_Culture_Lab_System/Tissue_Culture_Lab_System/content folder/pdmViewPlan.aspx.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,38 +9,9 @@ namespace Tissue_Culture_Lab_System.content_folder
 {
     public partial class pdmViewPlan : System.Web.UI.Page
     {
-        private string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
-        protected void pdmp1_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("pdmCreatePlan.aspx");
-        }
-
-        protected void planView_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("pdmUpdateDeletePlan.aspx");
-
-        }
-       
-
-
-
-
-
-
-
-
-
-            }
-
-        }
-
-
-    
-
-
-
+    }
+}
